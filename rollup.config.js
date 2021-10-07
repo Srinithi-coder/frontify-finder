ca9e6e35be0df2d1dc08301fd94cbfb7b600afad
Henrique Serro <henrique.serro@frontify.com>
2021-10-07 15:27:54 +0200

    Config and package management changes

--- rollup.config.js
+++ rollup.config.js
@@ -1,39 +1,47 @@
-// import commonjs from '@rollup/plugin-commonjs';
 import dts from 'rollup-plugin-dts';
 import esbuild from 'rollup-plugin-esbuild';
 
-const name = require('./package.json').main.replace(/\.js$/, '');
+const name = require('./package.json').main.replace('.umd.js', '');
 
 const bundle = (config) => ({
-  ...config,
-  input: 'src/index.ts',
-  external: (id) => !/^[./]/.test(id),
+    ...config,
+    input: 'src/index.ts',
+    external: (id) => !/^[./]/.test(id),
 });
 
 export default [
-  bundle({
-    plugins: [
-      // commonjs(),
-      esbuild(),
-    ],
-    output: [
-      {
-        file: `${name}.js`,
-        format: 'cjs',
-        sourcemap: true,
-      },
-      {
-        file: `${name}.mjs`,
-        format: 'es',
-        sourcemap: true,
-      },
-    ],
-  }),
-  bundle({
-    plugins: [dts()],
-    output: {
-      file: `${name}.d.ts`,
-      format: 'es',
-    },
-  }),
+    bundle({
+        plugins: [
+            // commonjs(),
+            esbuild({
+                minify: process.env.NODE_ENV === 'production',
+            }),
+        ],
+        output: [
+            {
+                file: `${name}.umd.js`,
+                format: 'umd',
+                name: 'FrontifyFinder',
+                sourcemap: true,
+            },
+            {
+                file: `${name}.es.js`,
+                format: 'es',
+                sourcemap: true,
+            },
+            {
+                file: `${name}.js`,
+                format: 'iife',
+                name: 'FrontifyFinder',
+                sourcemap: true,
+            },
+        ],
+    }),
+    bundle({
+        plugins: [dts()],
+        output: {
+            file: `${name}.d.ts`,
+            format: 'es',
+        },
+    }),
 ];
